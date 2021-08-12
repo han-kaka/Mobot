c1ba6a9a0658680e347f2094c9dfe098947f509f
hanqibin <hanqibin@qbb6.com>
2021-08-12 19:24:25 +0800

    1. 修改中断函数

--- ES32W3120_SDK_V1.00/Projects/ES32W3120/Applications/Examples_BLE_peripheral/LED_Server/Src/irq.c
+++ ES32W3120_SDK_V1.00/Projects/ES32W3120/Applications/Examples_BLE_peripheral/LED_Server/Src/irq.c
@@ -30,27 +30,19 @@
  */
 
 /* Includes ------------------------------------------------------------------ */
-
+#include "ald_conf.h"
 #include "bsp_mp6050.h"
-#include "md_utils.h"
 
 /* Exported Types ------------------------------------------------------------ */
+
 /* Exported Macros ----------------------------------------------------------- */
+
 /* Exported Variables -------------------------------------------------------- */
-//extern uint8_t g_complete;
-//extern md_dma_config_t g_dma_rx_config, g_dma_tx_config;
-//extern uint32_t g_adc_result;
+
+extern i2c_handle_t g_h_i2c;
 
 /* Exported Constants -------------------------------------------------------- */
-/* Exported Functions -------------------------------------------------------- */
 
-/** @addtogroup Projects_Examples_ALD
-  * @{
-  */
-
-/** @addtogroup Examples
-  * @{
-  */
 
 /**
   * @brief  NMI IRQ handler
@@ -142,85 +134,39 @@ void PendSV_Handler(void)
   */
 void SysTick_Handler(void)
 {
-    md_inc_tick();
+    ald_inc_tick();
     return;
 }
 
+#ifdef ALD_DMA
 /**
-  * @brief ADC_IRQHandler.
-  * @param none
-  * @retval none
-  */
-
-void ADC_IRQHandler(void)
-{
-//    if (md_adc_get_stat_nche(ADC) == 1)
-//    {
-//        md_adc_set_clr_nche(ADC);
-//        md_adc_set_clr_nchs(ADC);
-
-//        g_adc_result = md_adc_get_normal_channel_val(ADC) * 3320 / 4096;
-//    }
-}
-
-/**
-  * @brief  GP16C4T1 UPDATE IRQ handler
-  * @retval None
-  */
-void AD16C4T0_UP_IRQHandler(void)
-{
-//    /* update interrupt service program */
-//    if (md_timer_get_update_valid_interrupt_flag_uei(AD16C4T0) == 1)
-//    {
-//        md_timer_clear_update_interrupt_flag_ueic(AD16C4T0);
-
-//    }
-
-//    return;
-}
-
-/**
-  * @brief  I2C Event IRQ handler
-  * @retval None
-  */
-void I2C0_EV_IRQHandler(void)
-{
-    return;
-}
-
-/**
-  * @brief  I2C Error IRQ handler
-  * @retval None
-  */
-void I2C0_ERR_IRQHandler(void)
-{
-    return;
-}
-
-/**
-  * @brief  DMA IRQ handler
+  * @brief  DMA IRQ#66 handler
   * @retval None
   */
 void DMA_IRQHandler(void)
 {
-//    if (md_dma_is_active_flag_done(MD_DMA_CH_0)&&md_dma_is_enabled_it_done(MD_DMA_CH_0))
-//    {
-//        md_dma_clear_flag_done(MD_DMA_CH_0);
-//        md_dma_disable_it_done(MD_DMA_CH_0);
-//        md_i2c_set_stop(I2C1);
-//        g_complete = 1;
-//    }
-
-//    if (md_dma_is_active_flag_done(MD_DMA_CH_1)&&md_dma_is_enabled_it_done(MD_DMA_CH_1))
-//    {
-//        md_dma_clear_flag_done(MD_DMA_CH_1);
-//        md_dma_disable_it_done(MD_DMA_CH_1);
-//        md_i2c_set_stop(I2C1);
-//        g_complete = 1;
-//    }
-
-//    return;
+    ald_dma_irq_handler();
 }
+#endif /*ALD_DMA*/
+
+/**
+  * @brief  I2c Event IRQ handler
+  * @retval None
+  */
+void I2C1_EV_IRQHandler(void)
+{
+    ald_i2c_ev_irq_handler(&g_h_i2c);
+}
+
+/**
+  * @brief  I2c Error IRQ handler
+  * @retval None
+  */
+void I2C1_ERR_IRQHandler(void)
+{
+    ald_i2c_er_irq_handler(&g_h_i2c);
+}
+
 /**
   * @}
   */
